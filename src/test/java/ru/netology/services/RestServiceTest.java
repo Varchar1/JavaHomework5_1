e07c20cec5b95a53f3bedc7ec04df202000f74db
QA <test@neto.com>
2023-03-07 18:11:09 +0300

    CSV Version

--- src/test/java/ru/netology/services/RestServiceTest.java
+++ src/test/java/ru/netology/services/RestServiceTest.java
@@ -1,27 +1,17 @@
 package ru.netology.services;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class RestServiceTest {
-    @Test
-    public void RestTest1() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/rest.csv")
+    public void RestTest1(int expected, int income, int expenses, int threshold) {
         RestService service = new RestService();
 
-        int expected = 3;
-        int actual = service.calculate(10_000, 3_000, 20_000);
+        int actual = service.calculate(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
-
-    @Test
-    public void RestTest2() {
-        RestService service = new RestService();
-
-        int expected = 2;
-        int actual = service.calculate(100_000, 60_000, 150_000);
-
-        Assertions.assertEquals(expected, actual);
-    }
-
 }
